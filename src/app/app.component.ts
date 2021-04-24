85cda5a8bf5678b6263ef4c572943e47e26b936a
Benjamin Fallar III <bfallar3@gmail.com>
2021-04-24 21:07:50 +0800

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jen-jcpremier';
+  title = '@JENGUMABON';
+
+  goToktok() {
+    window.location.href = "http://www.toktok.ph/delivery/PPS15142560";
+  }
+
+  goMangBoks() {
+    window.location.href = "http://www.mangboks.ph/PPS15142560";
+  }
+
+  goBoyBondat() {
+    window.location.href = "http://www.boybondat.ph/ordernow/PPS15142560";
+  }
+
+  goSiomaiKing() {
+    window.location.href = "http://www.siomaiking.ph/ordernow/PPS15142560";
+  }
+
+  goCoppermask() {
+    window.location.href = "http://www.coppermask.ph/cpshop/PPS15142560";
+  }
+
+  goNinjaion() {
+    window.location.href = "http://www.ninjaph.com/cpshop/PPS15142560";
+  }
 }
