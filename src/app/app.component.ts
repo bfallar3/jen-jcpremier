8bf90490def5a5f200962307a84bbe4fdb203b7e
Benjamin Fallar III <bfallar3@gmail.com>
2021-06-19 23:45:18 +0800

    add toktok mall

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,34 +9,38 @@ export class AppComponent {
   title = '@JENGUMABON';
 
   goToktok() {
-    window.location.href = "http://www.toktok.ph/delivery/PPS15142560";
+    window.location.href = "http://www.toktok.ph/delivery/1111073097";
   }
 
   goMangBoks() {
-    window.location.href = "http://www.mangboks.ph/PPS15142560";
+    window.location.href = "http://www.mangboks.ph/1111073097";
   }
 
   goBoyBondat() {
-    window.location.href = "http://www.boybondat.ph/ordernow/PPS15142560";
+    window.location.href = "http://www.boybondat.ph/ordernow/1111073097";
   }
 
   goSiomaiKing() {
-    window.location.href = "http://www.siomaiking.ph/ordernow/PPS15142560";
+    window.location.href = "http://www.siomaiking.ph/ordernow/1111073097";
   }
 
   goCoppermask() {
-    window.location.href = "http://www.coppermask.ph/cpshop/PPS15142560";
+    window.location.href = "http://www.coppermask.ph/cpshop/1111073097";
   }
 
   goNinjaion() {
-    window.location.href = "http://www.ninjaph.com/cpshop/PPS15142560";
+    window.location.href = "http://www.ninjaph.com/cpshop/1111073097";
   }
 
   goShopForMe() {
-    window.location.href = "https://toktok.ph/pabiliservice/PPS15142560";
+    window.location.href = "https://toktok.ph/pabiliservice/1111073097";
   }
 
   goJcShop() {
-    window.location.href = "https://jcshop.ph/PPS15142560";
+    window.location.href = "https://jcshop.ph/1111073097";
+  }
+
+  goToktokmall() {
+    window.location.href = "https://www.toktokmall.ph/PPS15142560";
   }
 }
